8073736862b1920000d9dfa2b498e7a3d20c85ae
Irem Gokce Yildirim <yildiri4@msu.edu>
2016-09-14 12:35:43 -0400

    heroku changes reverted for tagging

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,19 +1,3 @@
-environments {
-	production {
-		dataSource {
-			dbCreate = "update"
-			driverClassName = "org.postgresql.Driver"
-			dialect = org.hibernate.dialect.PostgreSQL82Dialect
-
-			uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
-
-			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-			username = uri.userInfo.split(":")[0]
-			password = uri.userInfo.split(":")[1]
-		}
-	}
-}
-
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.crowdCausalTraining.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.crowdCausalTraining.UserRole'
