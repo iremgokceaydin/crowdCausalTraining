c2e4b56d0c5cde74e8960a5772166c04c882f1a6
Irem Gokce Yildirim <yildiri4@msu.edu>
2016-09-14 20:11:25 -0400

    heroku changes retry#2

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -5,9 +5,9 @@ environments {
 			driverClassName = "org.postgresql.Driver"
 			dialect = org.hibernate.dialect.PostgreSQL82Dialect
 
-			uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
+			uri = new URI(System.env.DATABASE_URL ?: "postgres://localhost:5432/test")
 
-			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require"
 			username = uri.userInfo.split(":")[0]
 			password = uri.userInfo.split(":")[1]
 		}
